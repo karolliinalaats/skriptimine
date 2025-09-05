cd2f992f8782573b512c6279afe699322ded04e5
karolliinalaats <karol-liina.laats@voco.ee>
2025-09-05 10:09:58 +0300

    Ülesanne 2: Loodud skript lause moodustamiseks.

---
+++ praks3/ylesanne2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# See skript loob muutujad ja ühendab need üheks lauseks.
+
+aasta=2016
+president="Kersti Kaljulaid"
+lause_keskosa=". aastal valiti Eesti presidendiks "
+lause="$aasta$lause_keskosa$president"
+
+echo "$lause"
