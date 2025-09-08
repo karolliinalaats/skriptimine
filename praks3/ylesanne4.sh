f9ff2389868f78e903f443d0613e9770be87221e
karolliinalaats <karol-liina.laats@voco.ee>
2025-09-08 14:30:10 +0300

    Skript bussi mahutavuse ja ülejäänud reisijate arvutamiseks.

--- praks3/ylesanne4.sh
+++ praks3/ylesanne4.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Küsib reisijate arvu
+# Küsi kasutajalt sisendit
 read -p "Sisesta reisijate arv: " reisijad
-
-# Küsib bussis olevate kohtade arvu
 read -p "Sisesta kohtade arv ühes bussis: " kohad
 
-# Arvutab täidetud busside arvu
-taidetud_bussid=$((reisijad / kohad))
+# Kontrolli, et mõlemad sisendid oleksid täisarvud
+if ! [[ "$reisijad" =~ ^[0-9]+$ ]] || ! [[ "$kohad" =~ ^[0-9]+$ ]]; then
+  echo "Viga: Palun sisesta ainult positiivsed täisarvud."
+  exit 1
+fi
 
-# Arvutab mahajäänud inimeste arvu
-mahajaanud_inimesed=$((reisijad % kohad))
+# Arvuta täis busse ja järelejäänud reisijad
+tais_busse=$(( reisijad / kohad ))
+ulejaanud=$(( reisijad % kohad ))
 
-# Väljastab tulemused
-echo "Täielikult täidetud busse: $taidetud_bussid"
-echo "Mahajäänud inimesi: $mahajaanud_inimesed"
+# Väljasta tulemus
+echo "Täielikult täidetud busse: $tais_busse"
+echo "Maha jääb inimesi: $ulejaanud"
+
